3a56b864c955b28a81c26bdbc5daec3282ce0d4b
Gabriela Galvan <gabriela1galvan@gmail.com>
2023-01-18 13:06:15 -0700

    completed arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name)
+{
+    cats.push(name);
+}
+;
+function destructivelyPrependCat(name)
+{
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name)
+{
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(name)
+{
+    cats.shift();
+}
+function appendCat(name)
+{
+    return appendCat = [...cats,name];
+}
+function prependCat(name)
+{
+    return prependCat = [name,...cats];
+}
+function removeFirstCat()
+{
+    return cats.slice(1);
+}
+function removeLastCat()
+{
+    return cats.slice(0,cats.length-1);
+}
